a085165523a32439d39bb3ec3bcad1391417f4ea
Stiware <stiwar-quintero@hotmail.com>
2024-04-24 11:00:00 -0500

    adding the error component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { CardsComponent } from './cards/cards.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
-    {path: 'ver', component: CardsComponent}
+    {path: '', redirectTo: 'ver', pathMatch: 'full'},
+    {path: 'ver', component: CardsComponent},
+    //ruta de error cuando no encuentra la pagina
+    {path: '**', component: ErrorComponent}
 ];
